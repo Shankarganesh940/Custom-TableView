1d42bae96d845f390e3e91593ff753404f6bc693
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-22 11:47:35 +0530

    1

--- CustomTablleView/ViewController.swift
+++ CustomTablleView/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
+{
+    var array = ["1", "2", "3", "4"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.label.text = "iOS"
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
